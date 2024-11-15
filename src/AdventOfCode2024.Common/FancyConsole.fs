0c1d9bab5f7b31bbfc8f483405136554d7304a18
Andy Blyth <andy@technicaldogsbody.com>
2024-11-15 16:55:24 +0000

    Added my fancy console from last year

---
+++ src/AdventOfCode2024.Common/FancyConsole.fs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode2024.Common
+
+open System.Diagnostics
+open Spectre.Console
+open Humanizer
+
+module FancyConsole =
+
+    let writeInfo (day: string) (functions: seq<string * (unit -> obj)>) =
+        // Display the Figlet text for the day
+        AnsiConsole.Write(FigletText(day).Color(Color.Green))
+
+        // Create and configure the table
+        let table =
+            Table()
+                .AddColumn("[bold]Name[/]")
+                .AddColumn("[bold]Result[/]")
+                .AddColumn("[bold]Time Taken[/]")
+                .Border(TableBorder.Rounded)
+
+        // Use Live Rendering with pipelining for clean flow
+        AnsiConsole.Live(table).Start(fun ctx ->
+            functions
+            |> Seq.iter (fun (name, func) ->
+                let sw = Stopwatch.StartNew()
+                let result = func()
+                sw.Stop()
+                
+                // Add a row with formatted text
+                table.AddRow(
+                    $"[yellow]{name}[/]",
+                    $"[cyan]{result}[/]",
+                    $"[green]{sw.Elapsed.Humanize()}[/]"
+                ) |> ignore
+
+                ctx.Refresh()
+            )
+        )
+
+
--- src/AdventOfCode2024.Day1/Program.fs
+++ src/AdventOfCode2024.Day1/Program.fs
@@ -1,12 +1,16 @@
 ﻿open System
 open AdventOfCode2024.Common
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let fileContent = FileService.getFileAsString("Test.txt")
 
-Console.WriteLine("Hello from Console")
+printfn "%s" fileContent
 
-printfn "%s" (FileService.getFileAsString("Test.txt"))
+let tasks = 
+    [ "Parse Input", (fun () -> fileContent :> obj)
+      "Calculate Part 1", (fun () -> 1234 :> obj)
+      "Calculate Part 2", (fun () -> 56789 :> obj) ]
+
+FancyConsole.writeInfo "Advent of Code - Day 1" tasks
 
 let input = Console.ReadLine()
 
